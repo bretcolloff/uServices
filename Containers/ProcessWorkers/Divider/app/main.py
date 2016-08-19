009b29f82ae4e497586a2eb8bd52662dd0c0f0a2
bretcolloff <bret.colloff@gmail.com>
2016-08-19 17:13:55 +0100

    Split the text data into pages properly
    Added page numbers and the title so each page can be processed
    individually.

--- Containers/ProcessWorkers/Divider/app/main.py
+++ Containers/ProcessWorkers/Divider/app/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import pika
 from itertools import zip_longest
+import json
 
 inputconnection = pika.BlockingConnection(pika.ConnectionParameters(host='dockermachine'))
 outputconnection = pika.BlockingConnection(pika.ConnectionParameters(host='dockermachine'))
@@ -13,17 +14,26 @@ output.queue_declare(queue='pageQueue')
 
 def grouper(iterable, n):
     args = [iter(iterable)] * n
-    return zip_longest(*args, fillvalue=' ')
+    return zip_longest(*args, fillvalue=" ")
 
 def callback(ch, method, properties, body):
     # Split the book into words...
-    words = body.decode('UTF-8').split(" ")
+    message = body.decode('UTF-8')
+    parsedMessage = json.loads(message)
+    words = parsedMessage["text"].split(" ")
+    title = parsedMessage["title"]
     # Split the book into pages...
     pages = grouper(words, 325)
 
+    pagenumber = 1
     # Add each page to the queue
     for page in pages:
-        output.basic_publish(exchange='', routing_key='pageQueue', body=" ".join(page))
+        pageMessage = {}
+        pageMessage["title"] = title
+        pageMessage["text"] = " ".join(page)
+        pageMessage["number"] = pagenumber
+        output.basic_publish(exchange='', routing_key='pageQueue', body=json.dumps(pageMessage))
+        pagenumber += 1
 
 input.basic_consume(callback,
                       queue='inputQueue',
