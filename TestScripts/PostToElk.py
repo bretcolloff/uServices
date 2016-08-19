3897568c25a2f1bff77196b3cd055bfe13fe7113
bretcolloff <bret.colloff@gmail.com>
2016-08-19 23:47:22 +0100

    Proving out and writing container for writing to ElasticSearch
    The test script is written and the code migrated, all needs tying
    together.
    Also renamed queue to testQueue as elasticsearch-py tries to load it.

--- TestScripts/PostToElk.py
+++ TestScripts/PostToElk.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python
-from elasticsearch import Elasticsearch
+import pika
 from itertools import zip_longest
 import json
-import pika
-import uuid
+from elasticsearch import Elasticsearch
 
-# Connect to ElasticSearch.
-es = Elasticsearch([{'host': 'dockermachine, 'port': 9200}])
+dockerHost = "10.82.53.41"
 
-# Connect to RabbitMq.
-inputconnection = pika.BlockingConnection(pika.ConnectionParameters(host='dockermachine'))
+es = Elasticsearch([{'host': dockerHost, 'port': 9200}])
 
-# Use the 'wordQueue' queue.
+inputconnection = pika.BlockingConnection(pika.ConnectionParameters(host=dockerHost))
 input = inputconnection.channel()
+
 input.queue_declare(queue='wordQueue')
 
-# Transform the message from the queue and send the output to ElasticSearch.
 def callback(ch, method, properties, body):
-    # Turn the string into JSON.
+    # Body will be json, contains: text, title, number
     message = body.decode('UTF-8')
     parsedMessage = json.loads(message)
 
-    # Take the details we want.
     title = parsedMessage["title"]
     word = parsedMessage["word"]
     wordIndex = parsedMessage["wordIndex"]
     pageNumber = parsedMessage["pageNumber"]
-    docId = str(uuid.uuid4())
+    docId = title + str(pageNumber) + str(wordIndex) + word
 
-    # Write the document to elasticsearch.
     es.index(index="words", doc_type="word", id=docId, body={"title": title, "word": word, "pageNumber": pageNumber, "wordIndex": wordIndex})
 
 input.basic_consume(callback,
                       queue='wordQueue',
                       no_ack=True)
 
-# Start watching the queue.
 input.start_consuming()
